0781fda00b48b9ed8029b864137c851591766462
Manny <casta.ma502@gmail.com>
2025-07-22 19:22:08 -0700

    Naked.

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -27,6 +27,12 @@ const firebaseConfig = {
 
 console.log("Vite mode:", import.meta.env.MODE);
 console.log("API key in use:", import.meta.env.VITE_FIREBASE_API_KEY);
+console.log("API key in use:", import.meta.env.VITE_FIREBASE_AUTH_DOMAIN);
+console.log("API key in use:", import.meta.env.VITE_FIREBASE_PROJECT_ID);
+console.log("API key in use:", import.meta.env.VITE_FIREBASE_STORAGE_BUCKET);
+console.log("API key in use:", import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID);
+console.log("API key in use:", import.meta.env.VITE_FIREBASE_APP_ID);
+console.log("API key in use:", import.meta.env.VITE_FIREBASE_MEASUREMENT_ID);
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);

02149c3baca745e24dec3ff5c5ef36bd9c6bb965
Manny <casta.ma502@gmail.com>
2025-07-22 20:13:53 -0700

    We got it working babyyy

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -26,13 +26,13 @@ const firebaseConfig = {
 };
 
 console.log("Vite mode:", import.meta.env);
-console.log("API key in use:", import.meta.env.VITE_FIREBASE_API_KEY);
-console.log("API key in use:", import.meta.env.VITE_FIREBASE_AUTH_DOMAIN);
-console.log("API key in use:", import.meta.env.VITE_FIREBASE_PROJECT_ID);
-console.log("API key in use:", import.meta.env.VITE_FIREBASE_STORAGE_BUCKET);
-console.log("API key in use:", import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID);
-console.log("API key in use:", import.meta.env.VITE_FIREBASE_APP_ID);
-console.log("API key in use:", import.meta.env.VITE_FIREBASE_MEASUREMENT_ID);
+// console.log("API key in use:", import.meta.env.VITE_FIREBASE_API_KEY);
+// console.log("API key in use:", import.meta.env.VITE_FIREBASE_AUTH_DOMAIN);
+// console.log("API key in use:", import.meta.env.VITE_FIREBASE_PROJECT_ID);
+// console.log("API key in use:", import.meta.env.VITE_FIREBASE_STORAGE_BUCKET);
+// console.log("API key in use:", import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID);
+// console.log("API key in use:", import.meta.env.VITE_FIREBASE_APP_ID);
+// console.log("API key in use:", import.meta.env.VITE_FIREBASE_MEASUREMENT_ID);
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
@@ -57,4 +57,3 @@ if (typeof window !== "undefined") {
 }
 
 export { db };
-window.firebaseDb = db;
